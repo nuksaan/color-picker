158bd32f28c0fc9f1e9f10181577377ed2be12f0
Muskan <muskanrangnani22@gmail.com>
2023-09-05 17:58:57 +0400

    New commit

--- app.js
+++ app.js
@@ -1,14 +1,31 @@
-const colors = ["red", "green", "orange", "yellow", "blue", "white", "black", "pink", "Turquoise", "magenta", "midnightblue"];
-const btn = document.getElementById("btn");
-const color = document.querySelector(".color");
+// const colors = ["red", "green", "orange", "yellow", "blue", "white", "black", "pink"];
+// const btn = document.getElementById('btn');
+// const color = document.querySelector('.color');
+
+// btn.addEventListener("click", function () {
+//     const randomNumber = getRandomNumber();
+//     document.body.style.backgroundColor = colors[randomNumber];
+//     color.textContent = colors[randomNumber];
+// })
+
+// function getRandomNumber() {
+//    return Math.getRandomNumber() * colors.length;
+// }
+
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, ,9, "A", "B", "C", "D", "E", "F"];
+const btn = document.getElementById('btn');
+const color = document.querySelector('.color');
 
 btn.addEventListener("click", function () {
-    const randomNumber = getRandomNumber();
-    document.body.style.backgroundColor = colors[randomNumber];
-    color.textContent = colors[randomNumber];
+    let hexColor = "#";
+    for (let i = 0; i < 6; i++) {
+        hexColor += hex[getRandomNumber()];
+    }
+
+    color.textContent = hexColor;
+    document.body.style.backgroundColor = hexColor;
 })
 
 function getRandomNumber() {
-    return Math.floor(Math.random() * colors.length);
-  }
-
+   return Math.floor(Math.random() * hex.length);
+}
